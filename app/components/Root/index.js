9cdaa055a9a528d6b308e8d8a519e572f17a060f
steve wirts <stevewirts@gmail.com>
2016-09-24 09:53:02 -0400

    font-awesome working

--- app/components/Root/index.js
+++ app/components/Root/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './style.css';
 import image from '../../../images/mrmeseeks.gif';
+import FontAwesome from 'react-fontawesome';
 
 export default class Root extends React.Component {
 
@@ -11,7 +12,15 @@ export default class Root extends React.Component {
 	render() {
 		return (
 			<div className={style.myStyle}>
-				<div>I'm Mr Meseeks!</div>
+				<div>
+					I'm Mr Meseeks!
+					<FontAwesome
+						className='super-crazy-colors'
+						name='rocket'
+						spin
+						style={{ textShadow: '0 2px 0 rgba(0, 0, 0, 0.3)' }}
+					/>
+				</div>
 				<img src={image}/>
 			</div>
 		)
